ef525297aa24f4f1346fbcc9e143f3ad76b5975c
maru536 <maru0218@naver.com>
2017-06-09 23:55:59 +0900

    Wifi Auto Connect

--- app/src/main/java/com/iotaddon/goout/ActivityDeviceSettingsConnectWIFI.java
+++ app/src/main/java/com/iotaddon/goout/ActivityDeviceSettingsConnectWIFI.java
@@ -1,15 +1,26 @@
 package com.iotaddon.goout;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout.LayoutParams;
 
 public class ActivityDeviceSettingsConnectWIFI extends AppCompatActivity implements View.OnClickListener{
 
-    Button btnNext;
+    private Button btnNext;
+    private ProgressBar mProgressBar;
+    private WifiManager mWifiManager;
+    final static String TAG = "ConnectWifiActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +28,11 @@ public class ActivityDeviceSettingsConnectWIFI extends AppCompatActivity impleme
         setContentView(R.layout.activity_device_settings_connect_wifi);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mWifiManager = (WifiManager)this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         btnNext = (Button)findViewById(R.id.activity_device_settings_connect_wifi_btn_next);
+
+        wifiAutoConnect(initWifiConfig("Wiznet_TestAP", "12345678"));
+
         btnNext.setOnClickListener(this);
     }
 
@@ -42,5 +57,36 @@ public class ActivityDeviceSettingsConnectWIFI extends AppCompatActivity impleme
                 break;
 
         }
+    }
+
+    private void wifiAutoConnect(WifiConfiguration wifiConfig) {
+        if (!mWifiManager.isWifiEnabled()) {
+            mWifiManager.setWifiEnabled(true);
+            while (!mWifiManager.isWifiEnabled()) {
+
+            }
+        }
+
+        int networkID = mWifiManager.addNetwork(wifiConfig);
+        if (networkID >= 0) {
+            mWifiManager.enableNetwork(networkID, true);
+        }
+    }
+
+    private WifiConfiguration initWifiConfig(String ssid, String pwd) {
+        WifiConfiguration wifiConfig = new WifiConfiguration();
+        wifiConfig.SSID = "\"".concat(ssid).concat("\"");
+        wifiConfig.status = WifiConfiguration.Status.DISABLED;
+        wifiConfig.priority = 40;
+        wifiConfig.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+        wifiConfig.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+        wifiConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+        wifiConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+        wifiConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+        wifiConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+        wifiConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+        wifiConfig.preSharedKey = "\"".concat(pwd).concat("\"");
+
+        return wifiConfig;
     }
 }
